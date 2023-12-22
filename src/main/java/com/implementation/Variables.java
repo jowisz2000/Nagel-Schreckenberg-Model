822edee15fedaeff7b4ea84a97c8d545300ddae5
Lukasz <lturynski@gmail.com>
2023-12-22 18:23:52 +0100

    variables made final

--- src/main/java/com/implementation/Variables.java
+++ src/main/java/com/implementation/Variables.java
@@ -2,17 +2,17 @@ package com.implementation;
 
 public class Variables {
     /**    side's length */
-    public static int sizeOfSquare = 20;
+    public static final int sizeOfSquare = 20;
     /**    length of left margin */
-    public static int leftMargin = 100;
+    public static final int leftMargin = 100;
     /**    length of upper margin */
-    public static int upperMargin = 150;
+    public static final int upperMargin = 150;
     /**    interval between neighbouring squares (fraction of side's length)  */
-    public static double interval = 0.1;
+    public static final double interval = 0.1;
     /** number of nodes in row */
-    public static int nodesInRow = 35;
+    public static final int nodesInRow = 35;
     /** number of nodes in column */
-    public static int nodesInColumn = 20;
+    public static final int nodesInColumn = 20;
     /** maximum number of points included in graphs */
-    public static int maxPointsInChart = 50;
+    public static final int maxPointsInChart = 50;
 }
